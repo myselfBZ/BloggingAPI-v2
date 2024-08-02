d3e63b021ad8866b38a426791f8c5532ad58303f
Boburmirzo <symonbobur@gmail.com>
2024-08-02 14:39:27 +0500

    Declaring types

--- types/types.go
+++ types/types.go
@@ -1,5 +1,16 @@
 package types
 
+type User struct{
+    ID          string  `json:"id" bson:"_id"`  
+    Email       string  `json:"email" bson:"email"`
+    Password    string  `json:"password" bson:"password"`
+    Name        string  `json:"name" bson:"name"`
+    LastName    string  `json:"lastName" bson:"lastName"`
+}
 
-
-
+type Blog struct{
+    UserId  string  `json:"userId" bson:"userId"`
+    ID      string  `json:"id" bson:"_id"`
+    Content string  `json:"content" bson:"content"`
+    Title   string  `json:"title" bson:"title"`
+}
